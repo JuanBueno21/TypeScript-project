2eaee92c8e2aa97dd3be52960d0a279c13ac817d
JuanBueno21 <juancamilobuenomintic@gmail.com>
2025-04-14 16:56:51 -0500

    configuracion de proyecto web con ts

---
+++ SitioWeb/public/scripts/main.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const h1 = document.querySelector("h1");
+console.log(h1 === null || h1 === void 0 ? void 0 : h1.textContent);
+let title = document.querySelector(".title");
+console.log(title === null || title === void 0 ? void 0 : title.textContent);
+const message = document.querySelector("#message");
+console.log(message.placeholder);
+console.log("hola mundo");
---
+++ SitioWeb/src/main.ts
@@ -0,0 +1,12 @@
+import { LogBox } from "react-native";
+
+const h1 = document.querySelector("h1");
+console.log(h1?.textContent);
+
+let title: HTMLHeadingElement = document.querySelector(".title") as HTMLHeadingElement;
+console.log(title?.textContent);
+
+const message = document.querySelector("#message") as HTMLInputElement;
+console.log(message.placeholder);
+
+console.log("hola mundo");
