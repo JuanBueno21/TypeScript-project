60af38461bc6c19b3783354cb7b2a6391c6ded81
JuanBueno21 <juancamilobuenomintic@gmail.com>
2025-04-14 16:11:31 -0500

    importacion y exportacion de modulos

---
+++ Modulos/main.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var calculator_1 = require("./calculator");
+var calculator_2 = require("./calculator");
+console.log((0, calculator_1.suma)(10, 32));
+console.log((0, calculator_1.resta)(30, 5));
+console.log((0, calculator_1.multiplicacion)(calculator_2.Pi, 1));
---
+++ Modulos/main.ts
@@ -0,0 +1,6 @@
+import { suma, resta, multiplicacion, division } from "./calculator"
+import { Pi } from "./calculator"
+
+console.log(suma(10, 32));
+console.log(resta(30, 5));
+console.log(multiplicacion(Pi, 1));
